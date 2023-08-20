d74187c2a1314bde9e6067c82394b857011c3602
anu singh <annusingh58@gmail.com>
2023-08-20 15:21:30 +0530

    updates useeffect counter

--- frontend/src/Components/Counter.jsx
+++ frontend/src/Components/Counter.jsx
@@ -6,6 +6,14 @@ function Counter(){
     const[flag,setFlag] = useState(false);
     console.log(counter,"-counter")
 
+    const changeFlag=()=>{
+        if(flag==false){
+            setFlag(true);
+        }else{
+            setFlag(false)
+        }
+    }
+
    return(
     <div>
         {flag ? <h1>flag is true</h1>:<h1>flag is false</h1>}
@@ -13,7 +21,7 @@ function Counter(){
         <button onClick={()=> setCounter((prevstate)=> prevstate + 1)}>+</button>
         <h2>Counter 2 -{counter2}</h2>
         <button onClick={()=>setCounter2((prevstate)=> prevstate + 1)}> +For 2 Counter</button>
-        <button onClick={()=> setFlag(true)}>true</button>
+        <button onClick={changeFlag}>change Flag</button>
     </div>
    
     
--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,9 +1,14 @@
 import { useNavigate } from "react-router-dom";
+import "../CSS/Login.css";
+
+
 
 function Login(){
     const router = useNavigate();
+
+    
     return(
-        <div>
+        <div id="screen">
            <h1>Login</h1>
            <button onClick={()=> router('/register')}>click here to go Register</button>
             <button onClick={()=>router('/')}>click here to go Home</button>
